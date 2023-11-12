a58686b60f4b80145c51cf39623ca47f59a34b94
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 15:57:07 +0700

    feat: add more content to QA

--- src/Qa.jsx
+++ src/Qa.jsx
@@ -5,6 +5,7 @@ export default function Qa() {
     <div className="qa_branch">
         <h1>Quality Assurance</h1>
         <p>Content of QA:</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, neque?</p>
     </div>
   )
 }
