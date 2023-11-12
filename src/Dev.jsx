af70fd6c4f426d6372e209e5c54f5614f6a4c943
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 15:51:48 +0700

    feat: add more content to dev_branch

--- src/Dev.jsx
+++ src/Dev.jsx
@@ -5,7 +5,9 @@ export default function Dev() {
   return (
     <div className='dev_branch'>
         <h1>Dev branch</h1>
-        <p>This is content of dev branch</p>
+        <p>This is content of dev branch:</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt itaque ratione officia. Accusantium sed soluta debitis placeat earum ipsam reiciendis! Dolor iure ullam assumenda a nisi autem cupiditate eum minus.
+        Blanditiis, beatae soluta perferendis aperiam doloremque error quod nesciunt. Debitis itaque dolore sequi illo. Esse ipsa totam, quidem eius ad possimus odio, reiciendis aliquid nobis consectetur animi accusantium, temporibus sit?</p>
         <p>Version 1.0 - create branch</p>
         <p>Version 1.1 - add .CSS file to dev_branch</p>
     </div>
