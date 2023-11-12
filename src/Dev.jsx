bbc3841480ee4c4d5bb12e232906c0a68f39c294
Bùi Như Phước <71402070+PhuocDev@users.noreply.github.com>
2023-11-12 16:46:19 +0700

    Feature2 (#1)
    * fea: add a new button named feature2 and have function alert message to user

--- src/Dev.jsx
+++ src/Dev.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Dev.css';
 import Feature1 from './Feature1';
+import Feature2 from './Feature2';
 
 export default function Dev() {
   return (
@@ -10,6 +11,7 @@ export default function Dev() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt itaque ratione officia. Accusantium sed soluta debitis placeat earum ipsam reiciendis! Dolor iure ullam assumenda a nisi autem cupiditate eum minus.
         Blanditiis, beatae soluta perferendis aperiam doloremque error quod nesciunt. Debitis itaque dolore sequi illo. Esse ipsa totam, quidem eius ad possimus odio, reiciendis aliquid nobis consectetur animi accusantium, temporibus sit?</p>
         <Feature1 />
+        <Feature2 />
         <p>Version 1.0 - create branch</p>
         <p>Version 1.1 - add .CSS file to dev_branch</p>
     </div>
