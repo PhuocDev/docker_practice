93687520d7d30958c1d9bedb140cde9b10e895e8
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 15:40:52 +0700

    complete set up git for project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Dev from './Dev';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Reacjs Practice with Docker</h1>
+      <Dev />
     </div>
   );
 }
---
+++ src/Dev.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Dev() {
+  return (
+    <>
+        <h1>Dev branch</h1>
+        <p>This is content of dev branch</p>
+        <p>Version 1.0</p>
+    </>
+  )
+}
