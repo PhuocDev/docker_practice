a6745ae2117f9456bbc3265e20fb060a9b3e9446
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 15:56:18 +0700

    feat: add QA component

---
+++ src/Qa.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Qa() {
+  return (
+    <div className="qa_branch">
+        <h1>Quality Assurance</h1>
+        <p>Content of QA:</p>
+    </div>
+  )
+}
