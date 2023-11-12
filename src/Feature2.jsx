e334edafff36b3452930133b1291269b265164b8
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 16:31:43 +0700

    fea: add color to feature 2

--- src/Feature2.jsx
+++ src/Feature2.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Feature2() {
   return (
-    <div>Feature2</div>
+    <div style={{color: "blue"}}>
+        <h5>This is feature 2
+            {/* <button onClick={() => alert("this is feature 1")}>F1</button> */}
+        </h5>
+    </div>
   )
 }
