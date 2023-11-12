a2c934b0d4f8e9678cf9d3d3850a1f9692714682
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 16:23:38 +0700

    feat: add btn to fea1

--- src/Feature1.jsx
+++ src/Feature1.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Feature1() {
   return (
     <div style={{color: "red"}}>
-        <h5>This is feature 1</h5>
+        <h5>This is feature 1 <button>F1</button></h5>
+
     </div>
   )
 }
