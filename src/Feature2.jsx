33052317cef4ac960e480fe0184c8f85d352ee11
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 16:32:26 +0700

    fea: add function to button of feature 2

--- src/Feature2.jsx
+++ src/Feature2.jsx
@@ -4,7 +4,7 @@ export default function Feature2() {
   return (
     <div style={{color: "blue"}}>
         <h5>This is feature 2
-            {/* <button onClick={() => alert("this is feature 1")}>F1</button> */}
+            <button onClick={() => alert("this is feature 2")}>F2</button>
         </h5>
     </div>
   )
