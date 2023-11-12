e841d246bc4801aaa87fa51eb78811b06f23a94f
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 23:48:53 +0700

    this branch is for deploy to docker hub

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ function App() {
   return (
     <div className="App">
       <h1>Reacjs Practice with Docker</h1>
-<<<<<<< HEAD
-      <h3>Production branch - THE MOST IMPORTANT BRANCH</h3>
-=======
       <Dev />
       <h5>Created by Phuocdev</h5>
->>>>>>> main
     </div>
   );
 }
