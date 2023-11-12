ca70143b3839ff64b69c4a5f68cfaa304bcc0555
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 16:24:29 +0700

    feat: add function to feature 1

--- src/Feature1.jsx
+++ src/Feature1.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Feature1() {
   return (
     <div style={{color: "red"}}>
-        <h5>This is feature 1 <button>F1</button></h5>
+        <h5>This is feature 1
+            <button onClick={() => alert("this is feature 1")}>F1</button>
+        </h5>
 
     </div>
   )
