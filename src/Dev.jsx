1f78a85e269b10c5e7798a05cf66d019014b8656
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 15:49:31 +0700

    feat: add bg color and padding to dev, changed to version 1.2

--- src/Dev.jsx
+++ src/Dev.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './Dev.css';
 
 export default function Dev() {
   return (
-    <>
+    <div className='dev_branch'>
         <h1>Dev branch</h1>
         <p>This is content of dev branch</p>
         <p>Version 1.0 - create branch</p>
-    </>
+        <p>Version 1.1 - add .CSS file to dev_branch</p>
+    </div>
   )
 }
