0a0358d35ccffb8667c13c6c2ab74320f044317a
phuocdev <phuocbuipro2903.pb@gmail.com>
2023-11-12 16:03:59 +0700

    feat: init new production branch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Reacjs Practice with Docker</h1>
-      <h3>Main branch - This is also production branch</h3>
+      <h3>Production branch - THE MOST IMPORTANT BRANCH</h3>
     </div>
   );
 }
